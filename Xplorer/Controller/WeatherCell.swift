c63d826653a4d900fe8b0d0b2bdefa6ce3470330
Joseph-Emmanuel <josephE.banzio@gmail.com>
2019-03-25 19:07:25 +0100

    cell

---
+++ Xplorer/Controller/WeatherCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCell.swift
+//  Xplorer
+//
+//  Created by Joseph-Emmanuel Banzio on 25/03/2019.
+//  Copyright © 2019 Joseph-Emmanuel Banzio. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+}
